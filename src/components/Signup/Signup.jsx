c3d445a62ba499bfaca5fef235e0c4bff7af9c6f
Viraj Maheshkumar Joshi <vr862603@dal.ca>
2023-02-24 17:41:46 -0400

    Added signup modal and modified login

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import SignupModal from '../SignupModal/SignupModal';
+import login_img from "../../assets/login.jpg";
 
 function SignUp() {
 
@@ -14,11 +17,13 @@ function SignUp() {
     const [emailError, setEmailError] = useState('');
     const [passwordError, setPasswordError] = useState('');
     const [confirmPasswordError, setConfirmPasswordError] = useState('');
+    const [blankFromError, setBlankFormError] = useState('');
 
     const nameRegEx = new RegExp('^[A-Za-z]+$');
     const validEmail = new RegExp('^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$');
 
     const [disabled, setDisabled] = useState(true);
+    const [showModal, setShowModal] = useState(false);
 
     const navigate = useNavigate();
 
@@ -115,63 +120,175 @@ function SignUp() {
     }
 
     const handleSubmit = (e) => {
-        console.log('Inside submit');
-        navigate('/login', { state: { fName: firstName, lName: lastName, em: email } });
+        e.preventDefault();
+        handleInputValidation(e);
+        console.log({ lastNameError });
+        if ({ firstNameError } || { lastNameError } || { email } ||
+            { password } || { confirmPasswordError }) {
+            setBlankFormError('Enter mandatory fields');
+        } else {
+            setBlankFormError('');
+            setFirstName('');
+            setLastName('');
+            setEmail('');
+            setPassword('');
+            setConfirmPassword('');
+            setShowModal(true);
+        }
+    }
+
+    const handleModalClose = e => {
+        setShowModal(false);
+        navigate('/login');
     }
 
     return (
-        <form className='form'>
-            <div className='formContent'>
-                <div className='formTitle'>
-                    <h3>Register</h3>
-                </div>
+        <StyledSignupImgWrapper className="login-img-wrapper">
+            <StyledForm className='form'>
+                <div className='formContent'>
+                    <div className='formTitle'>
+                        <h3>Register</h3>
+                    </div>
 
-                <div className='inputWrapper'>
-                    <label className='formLabel'>First Name*</label>
-                    <input className='formInput' type='text' name='firstName' value={firstName} onChange={(e) => handleInputChange(e)} onBlur={handleInputValidation} placeholder='First Name' />
-                </div>
-                <div className='err'>
-                    {<span className='err'>{firstNameError}</span>}
-                </div>
+                    <div className='inputWrapper'>
+                        <label className='formLabel'>First Name*</label>
+                        <input className='formInput' type='text' name='firstName' value={firstName} onChange={(e) => handleInputChange(e)} onBlur={handleInputValidation} placeholder='First Name' />
+                    </div>
+                    <div className='err'>
+                        {<span className='err'>{firstNameError}</span>}
+                    </div>
 
-                <div className='inputWrapper'>
-                    <label className='formLabel'>Last Name*</label>
-                    <input className='formInput' type='text' name='lastName' value={lastName} onChange={(e) => handleInputChange(e)} onBlur={handleInputValidation} placeholder='Last Name' />
-                </div>
-                <div className='err'>
-                    {<span className='err'>{lastNameError}</span>}
-                </div>
+                    <div className='inputWrapper'>
+                        <label className='formLabel'>Last Name*</label>
+                        <input className='formInput' type='text' name='lastName' value={lastName} onChange={(e) => handleInputChange(e)} onBlur={handleInputValidation} placeholder='Last Name' />
+                    </div>
+                    <div className='err'>
+                        {<span className='err'>{lastNameError}</span>}
+                    </div>
 
-                <div className='inputWrapper'>
-                    <label className='formLabel'>Email*</label>
-                    <input className='formInput' type='email' name='email' value={email} onChange={(e) => handleInputChange(e)} onBlur={handleInputValidation} placeholder='Email' />
-                </div>
-                <div className='err'>
-                    {<span className='err'>{emailError}</span>}
-                </div>
+                    <div className='inputWrapper'>
+                        <label className='formLabel'>Email*</label>
+                        <input className='formInput' type='email' name='email' value={email} onChange={(e) => handleInputChange(e)} onBlur={handleInputValidation} placeholder='Email' />
+                    </div>
+                    <div className='err'>
+                        {<span className='err'>{emailError}</span>}
+                    </div>
 
-                <div className='inputWrapper'>
-                    <label className='formLabel'>Password*</label>
-                    <input className='formInput' type='password' name='password' value={password} onChange={(e) => handleInputChange(e)} onBlur={handleInputValidation} placeholder='Password' />
-                </div>
-                <div className='err'>
-                    {<span className='err'>{passwordError}</span>}
-                </div>
+                    <div className='inputWrapper'>
+                        <label className='formLabel'>Password*</label>
+                        <input className='formInput' type='password' name='password' value={password} onChange={(e) => handleInputChange(e)} onBlur={handleInputValidation} placeholder='Password' />
+                    </div>
+                    <div className='err'>
+                        {<span className='err'>{passwordError}</span>}
+                    </div>
 
-                <div className='inputWrapper'>
-                    <label className='formLabel'>Confirm Password*</label>
-                    <input className='formInput' type='password' name='confirmPassword' value={confirmPassword} onChange={(e) => handleInputChange(e)} onBlur={handleInputValidation} placeholder='Confirm Password' />
-                </div>
-                <div className='err'>
-                    {<span className='err'>{confirmPasswordError}</span>}
-                </div>
+                    <div className='inputWrapper'>
+                        <label className='formLabel'>Confirm Password*</label>
+                        <input className='formInput' type='password' name='confirmPassword' value={confirmPassword} onChange={(e) => handleInputChange(e)} onBlur={handleInputValidation} placeholder='Confirm Password' />
+                    </div>
+                    <div className='err'>
+                        {<span className='err'>{confirmPasswordError}</span>}
+                    </div>
 
-                <div className='footNote'>
-                    <p>* Mandatory fields</p>
-                </div>
+                    <div className='footNote'>
+                        <p>* Mandatory fields</p>
+                    </div>
 
-                <button disabled={disabled} className='registerBtn' onClick={(e) => handleSubmit(e)}>Register</button>
-            </div>
-        </form>
+                    <div className='err'>
+                        {<span className='blank-err'>{blankFromError}</span>}
+                    </div>
+
+                    <button disabled={disabled} className='registerBtn' type='submit' onClick={(e) => handleSubmit(e)}>Register</button>
+                    <div className='submit-modal'>
+                        <SignupModal onClose={handleModalClose} showModal={showModal} />
+                    </div>
+                </div>
+            </StyledForm>
+            <StyledSignupImg className="login-img">
+                <img src={login_img} alt="login_img" />
+            </StyledSignupImg>
+        </StyledSignupImgWrapper>
     )
-} export default SignUp;
+}
+
+const StyledSignupImgWrapper = styled.div`
+    display: flex;
+    width: 60%;
+    box-shadow: 1px 1px 2px 2px #ccc;
+    border-radius: 5px;
+    margin: 1rem auto;
+    padding: 3rem;
+`
+
+
+const StyledSignupImg = styled.div`
+    flex-basis: 50%;
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`
+const StyledForm = styled.form`
+    margin:2rem auto;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    gap: 4rem;
+    padding: 0 1rem;
+    .formContent {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+    .inputWrapper{
+        display: flex;
+        font-size: 1.5rem;
+        flex-direction: column;
+        gap: 1rem;
+    }
+    .inputWrapper > input {
+        padding: 1rem;
+        outline: none;
+        border: none;
+        box-shadow: 1px 1px 2px 2px #ccc;
+        border-radius: 5px;
+    }
+    .err{
+    padding: 0rem;
+    color: red;
+    font-size: small;
+    text-align: right;
+    }
+    .registerBtn {
+    width: fit-content;
+    margin: 0 auto;
+    padding: 0.5rem;
+    background-color: rgb(16, 109, 240);
+    border-radius: 5px;
+    border:none;
+    box-shadow: 1px 1px 1px 1px #ccc;
+    color: white;
+    letter-spacing: 1px;
+    }
+    .registerBtn:hover{
+    cursor: pointer;
+    }
+    .registerBtn:disabled,
+    .registerBtn[disabled]{
+    cursor: not-allowed;
+    color: #666666;
+    background-color: #ccc;
+    }
+    .formTitle{
+    text-align: center;
+    font-size: 2rem;
+    }
+    .footNote{
+    color: red;
+    justify-content: left;
+    font-size: small;
+    padding: 0rem;
+    }
+`
+export default SignUp;
